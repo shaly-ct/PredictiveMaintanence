8d8b40bd9448c115b50fae8e039442bb349bc060
shaly-ct <57600351+shaly-ct@users.noreply.github.com>
2026-02-07 01:04:25 +0530

    Update prep.py

--- mlops/model_building/prep.py
+++ mlops/model_building/prep.py
@@ -11,7 +11,7 @@ from huggingface_hub import login, HfApi
 # Define constants for the dataset and output paths
 api = HfApi(token=os.getenv("HF_TOKEN"))
 DATASET_PATH = "hf://datasets/Shalyn/predictiveMaintanence/engine_data.csv"
-bank_dataset = pd.read_csv(DATASET_PATH)
+data = pd.read_csv(DATASET_PATH)
 print("Dataset loaded successfully.")
 
 
