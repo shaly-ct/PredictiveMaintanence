eb21ce00167470357dd15b2965921411d754801d
shaly-ct <shalyn.praise@gmail.com>
2026-02-06 17:20:58 +0000

    Add mlops subdirectory with project files

---
+++ mlops/hosting/hosting.py
@@ -0,0 +1,8 @@
+from huggingface_hub import HfApi
+import os
+api = HfApi(token=os.getenv("HF_TOKEN"))
+api.upload_folder(
+    folder_path="mlops/deployment",
+    repo_id="Shalyn/PredictiveMaintanence",
+    repo_type = "space"
+)
